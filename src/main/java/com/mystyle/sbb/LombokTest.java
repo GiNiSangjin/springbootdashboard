b39550630096d18de4f47be0365b7dfb6cdcc0e1
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-13 20:47:29 +0900

    롬복의 편리함을 느껴보자

--- src/main/java/com/mystyle/sbb/LombokTest.java
+++ src/main/java/com/mystyle/sbb/LombokTest.java
@@ -2,20 +2,19 @@ package com.mystyle.sbb;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 @Setter
-@Controller
+@RequiredArgsConstructor
 public class LombokTest {
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     public static void main(String[] args) {
-        LombokTest lombokTest = new LombokTest();
-        lombokTest.setHello("hello");
-        lombokTest.setLombok(5);
+        LombokTest lombokTest = new LombokTest("헬로", 5);
 
         System.out.println(lombokTest.getHello());
         System.out.println(lombokTest.getLombok());
