2b089f8657bc99cb2fb0662815aeb0c141636ca2
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-15 23:44:00 +0900

    db 연결 테스트

--- src/test/java/com/mystyle/sbb/SpringDashboardApplicationTests.java
+++ src/test/java/com/mystyle/sbb/SpringDashboardApplicationTests.java
@@ -1,6 +1,8 @@
 package com.mystyle.sbb;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +16,8 @@ class SpringDashboardApplicationTests {
 
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해 알고 싶습니다.");
-        q1.setCreateTime(LocalDateTime.now());
-        this.questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("스프링 부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateTime(LocalDateTime.now());
-        this.questionRepository.save(q2);
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 
 }
