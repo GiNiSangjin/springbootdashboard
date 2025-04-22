f247f1c0fa5b6194e77825c334d44391769abeff
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-22 09:27:13 +0900

    signup 중복 방지

--- src/main/java/com/mystyle/sbb/user/UserController.java
+++ src/main/java/com/mystyle/sbb/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -31,8 +32,17 @@ public class UserController {
             bindingResult.rejectValue("password2", "passwordIncorrect", "2개의 비밀번호가 일치하지 않습니다.");
             return "signup_form";
         }
-
+        try{
         userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
